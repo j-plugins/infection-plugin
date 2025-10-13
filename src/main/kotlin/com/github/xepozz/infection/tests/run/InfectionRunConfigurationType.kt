70d6605efd8f11f594468f46e388bd7022794067
Dmitriy Derepko <xepozz@list.ru>
2025-10-13 21:25:37 +0400

    chore: simplify class

--- src/main/kotlin/com/github/xepozz/infection/tests/run/InfectionRunConfigurationType.kt
+++ src/main/kotlin/com/github/xepozz/infection/tests/run/InfectionRunConfigurationType.kt
@@ -1,23 +1,26 @@
 package com.github.xepozz.infection.tests.run
 
+import com.github.xepozz.infection.InfectionBundle
 import com.github.xepozz.infection.InfectionIcons
-import com.intellij.execution.configurations.ConfigurationTypeBase
+import com.intellij.execution.configurations.ConfigurationTypeUtil.findConfigurationType
+import com.intellij.execution.configurations.SimpleConfigurationType
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.NotNullLazyValue
 
-class InfectionRunConfigurationType : ConfigurationTypeBase(
-    ID,
-    "Infection Test",
-    null,
-    InfectionIcons.INFECTION,
-) {
-    init {
-        addFactory(InfectionRunConfigurationFactory(this))
-    }
-
-    override fun getIcon() = InfectionIcons.INFECTION
+class InfectionRunConfigurationType :
+    SimpleConfigurationType(
+        ID,
+        InfectionBundle.message("infection.local.run.display.name"),
+        null,
+        NotNullLazyValue.createValue { InfectionIcons.INFECTION },
+    ) {
+    override fun createTemplateConfiguration(project: Project) =
+        InfectionRunConfiguration(project, this)
 
     companion object {
         const val ID = "InfectionRunConfiguration"
 
-        val INSTANCE = InfectionRunConfigurationType()
+        val INSTANCE
+            get() = findConfigurationType(InfectionRunConfigurationType::class.java)
     }
 }
