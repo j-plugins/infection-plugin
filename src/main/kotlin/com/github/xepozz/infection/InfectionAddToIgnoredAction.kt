dbc9d010aedf11974b2570a56e8903ccc3d40c71
Dmitriy Derepko <xepozz@list.ru>
2026-01-21 20:42:00 +0400

    refactor: remove "quality tools" stuff

--- src/main/kotlin/com/github/xepozz/infection/InfectionAddToIgnoredAction.kt
+++
@@ -1,8 +0,0 @@
-package com.github.xepozz.infection
-
-import com.intellij.openapi.project.Project
-import com.jetbrains.php.tools.quality.QualityToolAddToIgnoredAction
-
-class InfectionAddToIgnoredAction : QualityToolAddToIgnoredAction() {
-    override fun getQualityToolType(project: Project?) = InfectionQualityToolType.INSTANCE
-}
