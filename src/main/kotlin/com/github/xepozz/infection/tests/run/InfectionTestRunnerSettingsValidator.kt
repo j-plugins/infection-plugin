5f34a63b33f385de7f7e938f6753383145414e14
Dmitriy Derepko <xepozz@list.ru>
2026-01-23 12:42:20 +0400

    fix: methods validator

--- src/main/kotlin/com/github/xepozz/infection/tests/run/InfectionTestRunnerSettingsValidator.kt
+++ src/main/kotlin/com/github/xepozz/infection/tests/run/InfectionTestRunnerSettingsValidator.kt
@@ -1,14 +1,19 @@
 package com.github.xepozz.infection.tests.run
 
 import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.util.asSafely
 import com.jetbrains.php.lang.PhpFileType
+import com.jetbrains.php.lang.psi.PhpFile
+import com.jetbrains.php.lang.psi.elements.Function
 import com.jetbrains.php.testFramework.run.PhpDefaultTestRunnerSettingsValidator
 
 object InfectionTestRunnerSettingsValidator: PhpDefaultTestRunnerSettingsValidator(
     listOf(PhpFileType.INSTANCE),
-    PhpTestMethodFinder { file: PsiFile, sss: String ->
-        println("file: $file: strings: $sss")
-        true
+    PhpTestMethodFinder { file: PsiFile, name: String ->
+        file.asSafely<PhpFile>()
+            .let { PsiTreeUtil.findChildrenOfType(it, Function::class.java) }
+            .any { it.name == name }
     },
     false,
     false,
